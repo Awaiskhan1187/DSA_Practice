30f9db5cc3aff119521fa3dc9b29e5f28d34d38e
Awaiskhan1187 <Awaiskhanpirpiai@gmail.com>
2025-11-08 19:53:46 +0500

    After some changes IN bubble sort

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 void bubblesort(int arr[],int n){
-    for(int i = 0; i < n -2; i++){
-        for(int j = 0; j < n - i -2; j++){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i -1; j++){
             if(arr[j] >= arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
@@ -15,7 +15,7 @@ void bubblesort(int arr[],int n){
 
 int main(){
 
-    int arr[] = {2,5,1,6,7};
+    int arr[] = {2,5,1,7,0};
     bubblesort(arr,5);
     for(int i: arr) cout<<i<<" ";
     cout<<endl;
